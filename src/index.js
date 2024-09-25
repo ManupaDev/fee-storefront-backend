7d91728752001b7d8d20777430d4301e83b6e3c5
ManupaDev <manuapdevsuite@gmail.com>
2024-09-26 02:21:45 +0530

    feat: update

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: "https://fee-storefront-frontend.netlify.app"}));
+app.use(cors());
 
 app.use("/api/products", productsRouter);
 app.use("/api/categories", categoriesRouter);
